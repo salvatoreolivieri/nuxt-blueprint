b157fb3ab98ae437edb570be5e614d364530aff6
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2023-07-09 13:37:26 +0200

    chore(plugin): implementing custom plugin

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { $hello } = useNuxtApp()
+</script>
 
 <template>
   <div>
     <h1>Index</h1>
+
+    <!-- Icon -->
     <Icon name="uil:github" color="black" />
+
+    <!-- Plugin -->
+    <p>
+      {{ $hello("custom message") }}
+    </p>
   </div>
 </template>
 
