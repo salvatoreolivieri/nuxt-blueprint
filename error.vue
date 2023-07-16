7a226eb02f56cf87625caa6536d44e9fb9d9427b
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2023-07-16 17:04:01 +0200

    chore: add useError in error page

--- error.vue
+++ error.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const error = useError()
+</script>
 
 <template>
   <div>
     <p>pagina 404</p>
+    <pre>
+      {{ error }}
+    </pre>
   </div>
 </template>
 
